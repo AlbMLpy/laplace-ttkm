68dc4f72fbf198724d403feaff7f8b6268eb0ff1
Albert Saiapin <analystalb@gmail.com>
2025-11-07 13:31:40 +0100

    Added Grad and Loss tracker

--- source/trackers.py
+++ source/trackers.py
@@ -1,5 +1,8 @@
 from collections import defaultdict
 
+from .evaluation import norm_frob
+from .matrix_operations import tt2vec
+from .model_functionality_tt import get_tt_ranks
 from .general_functions import update_results_dict
 
 class LossTracker:
@@ -11,11 +14,37 @@ class LossTracker:
         self.gamma_w = gamma_w
         self.loss = loss
 
-    def track(self, w_tt, kd, fmap):
+    def track(self, w_tt, kd, fmap, beta_e=None, gamma_w=None):
+        _beta_e = beta_e if beta_e else self.beta_e
+        _gamma_w = gamma_w if gamma_w else self.gamma_w
+
         train_loss = self.loss(
             w_tt, kd, self.x_train, self.y_train, 
-            fmap, self.gamma_w, self.beta_e
-        )
+            fmap, _gamma_w, _beta_e
+        ).item()
+        update_results_dict(self.res_dict, loss=train_loss)
+
+class GradTracker(LossTracker):
+    def __init__(self, x_train, y_train, beta_e, gamma_w, loss, grad_w):
+        super().__init__(x_train, y_train, beta_e, gamma_w, loss)
+        self.grad_w = grad_w
+
+    def track(self, w_tt, kd, fmap, beta_e=None, gamma_w=None):
+        _beta_e = beta_e if beta_e else self.beta_e
+        _gamma_w = gamma_w if gamma_w else self.gamma_w
+
+        w_vec = tt2vec(w_tt)
+        m_order, rank_list = w_tt[0].shape[1], get_tt_ranks(w_tt)
+        train_loss = self.loss(
+            w_tt, kd, self.x_train, self.y_train, 
+            fmap, _gamma_w, _beta_e
+        ).item()
+        grad_norm = norm_frob(
+            self.grad_w(
+                w_vec, kd, self.x_train, self.y_train, 
+                fmap, _gamma_w, _beta_e, m_order, rank_list,
+            )
+        ).item()
         update_results_dict(
-            self.res_dict, loss=train_loss
+            self.res_dict, loss=train_loss, grad_norm=grad_norm,
         )
